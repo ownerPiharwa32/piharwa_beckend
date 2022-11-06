b3acfff8fe38e4f7e93c7ebd7112249571814364
vamit7631 <vamit7631@gmail.com>
2022-11-06 19:49:37 +0530

    update database path

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
     },
     database: {
       //  url: `mongodb://localhost:27017/piharwaDB`,
-      url: `mongodb+srv://piharwaDB:piharwaDBpass@cluster0.mx6skd3.mongodb.net/?retryWrites=true&w=majority`,
+      url: `mongodb+srv://piharwaDB:piharwaDBpass@cluster0.mx6skd3.mongodb.net/piharwaDB?retryWrites=true&w=majority`,
     },
 
   whitelist: { whitelist_url: ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:8080'] },
