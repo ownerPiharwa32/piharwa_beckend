2d44d74c9218dc87bb36efb9a8a51f46a024b783
vamit7631 <vamit7631@gmail.com>
2022-12-25 00:14:53 +0530

    update config file

--- config/config.js
+++ config/config.js
@@ -5,10 +5,10 @@ const config = {
     swaggerHostname: process.env.SWAGGER_HOSTNAME
   },
   database: {
-      // url: `mongodb://localhost:27017/piharwaDB`,
-      url: `mongodb+srv://piharwaDB:piharwaDBpass@cluster0.mx6skd3.mongodb.net/piharwaDB?retryWrites=true&w=majority`
+      //  url: `mongodb://localhost:27017/piharwaDB`,
+       url: `mongodb+srv://piharwaDB:piharwaDBpass@cluster0.mx6skd3.mongodb.net/piharwaDB?retryWrites=true&w=majority`
   },
-  whitelist: { whitelist_url: ['http://localhost:3000', 'http://localhost:3001','http://localhost:4200', 'http://localhost:8080', 'http://piharwa.co.in', 'http://3.22.167.33:3000' ] },
+  whitelist: { whitelist_url: ['http://localhost:3000', 'http://localhost:3001','http://localhost:4200', 'http://localhost:8080', 'http://piharwa.co.in', 'http://3.22.167.33:3000','http://www.piharwa.com','http://piharwa.com' ] },
     
   jwt: {
     jwtSecret: process.env.JWT_SECRET_TOKEN,
