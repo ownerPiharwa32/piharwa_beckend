af62e5ca977ca213585e55563a5b99abb6ef2998
vamit7631 <vamit7631@gmail.com>
2023-01-28 16:59:39 +0530

    update changes

--- services/cart-service.js
+++ services/cart-service.js
@@ -8,11 +8,23 @@ module.exports.addProductInCart = async (reqUser, reqBody) => {
     let user_id = ObjectId(reqUser.user_id)
     let productDetails = reqBody.productDetails
     let cartArr = [];
+    let result;
     for (let i = 0; i < productDetails.length; i++) {
         const productItems = productDetails[i];
         productItems.user_id = user_id;
-        let result = await productCartModel.create(productItems)
-        cartArr.push(result)
+        let productId = productItems.productId
+        let existingProduct = await productCartModel.find({ user_id: ObjectId(user_id), productId: ObjectId(productId) })
+        
+        if (existingProduct.length > 0) {
+            result = await productCartModel.findOneAndUpdate({ user_id: ObjectId(user_id), productId: ObjectId(productId) }, { $set: { quantity: productItems.quantity, sizes: productItems.sizes } })
+            let existingProduct1 = await productCartModel.find({ user_id: ObjectId(user_id), productId: ObjectId(productId) })
+            cartArr.push(existingProduct1)
+        } else {
+            result = await productCartModel.create(productItems)
+            cartArr.push(result)
+        }
+        
+        // cartArr.push(result)
     }
 
     return {
