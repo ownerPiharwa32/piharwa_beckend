24bda5d0cc4ba320d12db5ddbcf6fa90e372f2e7
vamit7631 <vamit7631@gmail.com>
2023-02-09 20:54:30 +0530

    added fixes

--- config/multer-config.js
+++ config/multer-config.js
@@ -6,33 +6,37 @@ const config = require("./config")
 const path = require("path")
 
 let s3 = new aws.S3({
-      region: config.s3Client.region,
-      accessKeyId: config.s3Client.AWSKEYACCESSID,
-      secretAccessKey: config.s3Client.AWSSECRETKEY
+  region: config.s3Client.region,
+  accessKeyId: config.s3Client.AWSKEYACCESSID,
+  secretAccessKey: config.s3Client.AWSSECRETKEY
 
-  });
+});
 
 
 module.exports.uploadFile = multer({
-    dest: "products/",
-    storage: multerS3({
-        s3: s3,
-        bucket: config.s3Client.bucket,
-        ACL: 'public-read',
-        metadata: function (req, file, cb) {
-          cb(null, { fieldName: file.originalname });
-        },
-    key: function (req, file, cb) { 
-            let extType = file.mimetype.split('/')[0];
-            if (extType == "image") { 
-              const ext = file.mimetype.split('/')[1];
-              const newFileName = Date.now() + "-" + file.originalname;
-              const fullPath = `products/${req.params.productId}/images/${newFileName}`
-              cb(null, fullPath);
-            }
-        }
+  dest: "products/",
+  limits: { fileSize: 10 * 1000 * 1000 }, // 10MB max file size
+  storage: multerS3({
+    s3: s3,
+    bucket: config.s3Client.bucket,
+    ACL: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.originalname });
+    },
+    key: function (req, file, cb) {
+      const fileSize = parseInt(req.headers["content-length"])
+      let extType = file.mimetype.split('/')[0];
+      if (extType == "image" && fileSize <= 10000000) {
+          const ext = file.mimetype.split('/')[1];
+          const newFileName = Date.now() + "-" + file.originalname;
+          const fullPath = `products/${req.params.productId}/images/${newFileName}`
+          cb(null, fullPath);
+      } else {
+        return cb("File size is too large");
+      }
+    }
   })
-  
- 
+
+
 
 })
